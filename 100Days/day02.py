1be210e6250ea37a895497eaffa5363022a6571d
D3rk1us <derkius1@gmail.com>
2025-02-27 15:29:59 -0500

    Second day, done.

---
+++ 100Days/day02.py
@@ -0,0 +1,21 @@
+'''
+myName = input("What's your name?: ")
+myAge = input("How old are you? : ")
+print("Gee, that's REALLY OLD")
+print()
+print("So, you are")
+print(myName)
+print("and your age is")
+print(myAge)
+'''
+print("Getting to know you!\n")
+name = input("What's your name?: ")
+food = input("What's your favourite food?: ")
+music = input("What's your favourite music?: ")
+address = input("Where do you live?: ")
+print()
+
+print("You are\n" + name)
+print("You're probably hungry for\n" + food)
+print("and you're definitely getting your groove on to\n" + music)
+print("living in the amazing\n" + address)
