361db65d2e5facd26e8991e34fb576150a8cb4bd
Erickson Sterling Peña <97615595+D3rk1us@users.noreply.github.com>
2025-03-02 23:54:34 +0100

    Update day03.py

--- 100Days/day03.py
+++ 100Days/day03.py
@@ -1,5 +1,21 @@
 """
  * EJERCICIO:
+ * - Muestra ejemplos de creación de todas las estructuras soportadas por defecto en tu lenguaje.
+ * - Utiliza operaciones de inserción, borrado, actualización y ordenación.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Crea una agenda de contactos por terminal.
+ * - Debes implementar funcionalidades de búsqueda, inserción, actualización y eliminación de contactos.
+ * - Cada contacto debe tener un nombre y un número de teléfono.
+ * - El programa solicita en primer lugar cuál es la operación que se quiere realizar, y a continuación
+ *   los datos necesarios para llevarla a cabo.
+ * - El programa no puede dejar introducir números de teléfono no númericos y con más de 11 dígitos.
+ *   (o el número de dígitos que quieras)
+ * - También se debe proponer una operación de finalización del programa.
+ """
+
+"""
+ * EJERCICIO:
  * - Crea ejemplos de funciones básicas que representen las diferentes
  *   posibilidades del lenguaje:
  *   Sin parámetros ni retorno, con uno o varios parámetros, con retorno...
@@ -19,4 +35,62 @@
  *
  * Presta especial atención a la sintaxis que debes utilizar en cada uno de los casos.
  * Cada lenguaje sigue una convenciones que debes de respetar para que el código se entienda.
- """
+"""
+
+# Función sin parámetros, ni retorno
+
+def firstFunction():
+    print('Hola mundo.')
+
+firstFunction()
+
+# Función con un parámetro, con variables locales y utilizando una función ya creada por el lenguaje.
+
+def circleArea(r):
+    PI = 3.141592
+    result = PI * r**2
+    print(round(result, 2))
+
+circleArea(2)
+
+# Función con retorno y con una variable global.
+
+num = 2
+
+def returnFunction():
+    return num * num
+
+print(returnFunction())
+
+# Función con un parámetro y retorno.
+
+def welcome(name):
+    return f"Bienvenido/a, {name}."
+
+print(welcome("Python"))
+
+# Función creada dentro de otra función
+
+def rectangleArea(b, a):
+    def calculate():
+        return b * a
+
+    return calculate()
+
+print(rectangleArea(10, 5))
+
+# Dificultad Extra
+"""
+ * DIFICULTAD EXTRA (opcional):
+ * Crea una función que reciba dos parámetros de tipo cadena de texto y retorne un número.
+ * - La función imprime todos los números del 1 al 100. Teniendo en cuenta que:
+ *   - Si el número es múltiplo de 3, muestra la cadena de texto del primer parámetro.
+ *   - Si el número es múltiplo de 5, muestra la cadena de texto del segundo parámetro.
+ *   - Si el número es múltiplo de 3 y de 5, muestra las dos cadenas de texto concatenadas.
+ *   - La función retorna el número de veces que se ha impreso el número en lugar de los textos.
+ *
+ * Presta especial atención a la sintaxis que debes utilizar en cada uno de los casos.
+ * Cada lenguaje sigue una convenciones que debes de respetar para que el código se entienda.
+"""
+
+
