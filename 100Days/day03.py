f9f7fb6495155257472820e425f1617d98179f04
Erickson Sterling Peña <97615595+D3rk1us@users.noreply.github.com>
2025-03-03 01:15:04 +0100

    Update day03.py

--- 100Days/day03.py
+++ 100Days/day03.py
@@ -1,21 +1,5 @@
 """
  * EJERCICIO:
- * - Muestra ejemplos de creación de todas las estructuras soportadas por defecto en tu lenguaje.
- * - Utiliza operaciones de inserción, borrado, actualización y ordenación.
- *
- * DIFICULTAD EXTRA (opcional):
- * Crea una agenda de contactos por terminal.
- * - Debes implementar funcionalidades de búsqueda, inserción, actualización y eliminación de contactos.
- * - Cada contacto debe tener un nombre y un número de teléfono.
- * - El programa solicita en primer lugar cuál es la operación que se quiere realizar, y a continuación
- *   los datos necesarios para llevarla a cabo.
- * - El programa no puede dejar introducir números de teléfono no númericos y con más de 11 dígitos.
- *   (o el número de dígitos que quieras)
- * - También se debe proponer una operación de finalización del programa.
- """
-
-"""
- * EJERCICIO:
  * - Crea ejemplos de funciones básicas que representen las diferentes
  *   posibilidades del lenguaje:
  *   Sin parámetros ni retorno, con uno o varios parámetros, con retorno...
@@ -79,18 +63,39 @@ def rectangleArea(b, a):
 
 print(rectangleArea(10, 5))
 
-# Dificultad Extra
-"""
- * DIFICULTAD EXTRA (opcional):
- * Crea una función que reciba dos parámetros de tipo cadena de texto y retorne un número.
- * - La función imprime todos los números del 1 al 100. Teniendo en cuenta que:
- *   - Si el número es múltiplo de 3, muestra la cadena de texto del primer parámetro.
- *   - Si el número es múltiplo de 5, muestra la cadena de texto del segundo parámetro.
- *   - Si el número es múltiplo de 3 y de 5, muestra las dos cadenas de texto concatenadas.
- *   - La función retorna el número de veces que se ha impreso el número en lugar de los textos.
- *
- * Presta especial atención a la sintaxis que debes utilizar en cada uno de los casos.
- * Cada lenguaje sigue una convenciones que debes de respetar para que el código se entienda.
-"""
+# Dificultad Extra:
+
+# Crea una función que reciba dos parámetros de tipo cadena de texto y retorne un número.
+def fizzBuzz(str1, str2):
+    
+    if isinstance(str1, str) and isinstance(str2, str):
+
+        total_times = 0
+
+        # La función imprime todos los números del 1 al 100.
+        for i in range(1,101):
+
+            # Si el número es múltiplo de 3 y de 5, muestra las dos cadenas de texto concatenadas.
+            if i % 5 == 0 and i % 3 == 0:
+                print(str1 + str2)
+            
+            # Si el número es múltiplo de 5, muestra la cadena de texto del segundo parámetro.
+            elif i % 5 == 0:
+                print(str2)
+            
+            # Si el número es múltiplo de 3, muestra la cadena de texto del primer parámetro.
+            elif i % 3 == 0:
+                print(str1)
+
+            else:
+                total_times += 1
 
 
+        # La función retorna el número de veces que se ha impreso el número en lugar de los textos.
+        return total_times
+    
+    else:
+        return "Se tiene que indicar por parámetro dos cadenas de texto(Por ejemplo: 'Fizz', 'Buzz'). Inténtalo de nuevo..."
+
+
+print(fizzBuzz("Fizz", "Buzz"))
