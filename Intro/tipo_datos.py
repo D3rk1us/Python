8424b094d201baaf8642e5c25e913b4b12a3250f
D3rk1us <derkius1@gmail.com>
2026-02-07 18:52:12 -0500

    Tipo de datos.

---
+++ Intro/tipo_datos.py
@@ -0,0 +1,29 @@
+# String
+
+cadena = "¡Te doy la bienvenida!"
+
+# Integer
+numero = 145
+
+# Float
+
+precio = 10.4
+
+# Boolean
+
+encendido = True
+
+# List
+
+listado = ['Arroz', 'Pan', 'Huevo']
+
+# Dictionary
+
+{'id': 10203, 'nombre': 'TV Samsumg', 'precio': 150.99}
+
+# Tupla
+
+punto_geografico = (40.4167, -3.7032)
+
+
+
