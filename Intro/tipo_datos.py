b4de8428f70f37c68afc9b133c2d85964e01c4be
D3rk1us <derkius1@gmail.com>
2026-03-24 06:57:10 -0400

    Updating data types.

--- Intro/tipo_datos.py
+++ Intro/tipo_datos.py
@@ -1,29 +1,29 @@
 # String
-
 cadena = "¡Te doy la bienvenida!"
+print('String:', cadena)
 
 # Integer
 numero = 145
+print('Integer:', numero)
 
 # Float
-
 precio = 10.4
+print('Float:', precio)
 
 # Boolean
-
 encendido = True
+print('Boolean:', encendido)
 
 # List
-
 listado = ['Arroz', 'Pan', 'Huevo']
+print('List:', listado)
 
 # Dictionary
-
-{'id': 10203, 'nombre': 'TV Samsumg', 'precio': 150.99}
+mi_diccionario = {'id': 10203, 'nombre': 'TV Samsumg', 'precio': 150.99}
+print('Dictionary:', mi_diccionario)
 
 # Tupla
-
 punto_geografico = (40.4167, -3.7032)
-
+print('Tuple:', punto_geografico)
 
 
