fe226efc98515dccaecc115602c6fcf7f3f5f68d
D3rk1us <derkius1@gmail.com>
2026-04-07 10:11:57 -0400

    Files update.

---
+++ basics/data_type.py
@@ -0,0 +1,29 @@
+# String
+welcome = "Welcome to my repository!"
+print('String:', welcome)
+
+# Integer
+number = 145
+print('Integer:', number)
+
+# Float
+price = 10.4
+print('Float:', price)
+
+# Boolean
+on_off = True
+print('Boolean:', on_off)
+
+# List
+recipe = ['Rice', 'salt', 'eggs']
+print('List:', recipe)
+
+# Dictionary
+my_dictionary = {'id': 10203, 'name': 'TV Samsumg', 'price': 150.99}
+print('Dictionary:', my_dictionary)
+
+# Tupla
+geographical_point = (40.4167, -3.7032)
+print('Tuple:', geographical_point)
+
+
