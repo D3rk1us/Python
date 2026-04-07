d3b913208fe093659b73d1dce6a0d9e6f7bc5b2a
D3rk1us <derkius1@gmail.com>
2026-04-06 23:42:49 -0400

    Loops notes.

---
+++ basics/loops_sequences.py
@@ -0,0 +1,25 @@
+
+# For loop
+programming_languages = ['Rust', 'Java', 'Python', 'C++']
+
+for language in programming_languages:
+    print(language)
+
+categories = ['Fruit', 'Vegetable']
+foods = ['Apple', 'Carrot', 'Banana']
+
+for category in categories:
+    for food in foods:
+        print(category, food)
+
+# While loop
+secret_number = 3
+guess = 0
+
+while guess != secret_number:
+    guess = int(input('Guess the number (1-5): '))
+    
+    if guess != secret_number:
+        print('Wrong! Try again.')
+
+print('You got it!')
