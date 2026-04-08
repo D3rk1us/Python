340d2982d54b2c2faf27a5d7b4b74c6cfe38b585
D3rk1us <derkius1@gmail.com>
2026-04-08 11:18:24 -0400

    Sequences.

--- basics/loops_sequences.py
+++ basics/loops_sequences.py
@@ -12,6 +12,21 @@ for category in categories:
     for food in foods:
         print(category, food)
 
+# range(start, stop, steps)
+for num in range(1, 5):
+    print(num)
+
+for index, language in enumerate(programming_languages):
+    print(f'Index {index} and language {language}')
+
+developers = ['Naomi', 'Dario', 'Jessica', 'Tom']
+ids = [1, 2, 3, 4]
+
+for name, id in zip(developers, ids):
+    print(f'Name: {name}')
+    print(f'ID: {id})
+
+
 # While loop
 secret_number = 3
 guess = 0
